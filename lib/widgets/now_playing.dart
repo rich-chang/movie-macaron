a6bd666f2587f75348249879c303f1b0d376938c
Rich@Neurobonic <rich@Neurobonic.com>
2022-09-07 15:04:21 +0800

    Init NowPlaying screen

---
+++ lib/widgets/now_playing.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class NowPlaying extends StatefulWidget {
+  const NowPlaying({required Key key}) : super(key: key);
+
+  @override
+  State<NowPlaying> createState() => _NowPlayingState();
+}
+
+class _NowPlayingState extends State<NowPlaying> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
