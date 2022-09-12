a8570afccd209f7da713de349217946de6ed6d08
Rich@Neurobonic <rich@Neurobonic.com>
2022-09-12 15:39:37 +0800

    Implement logic part of NowPlaying on HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_macaron/widgets/now_playing.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,27 +11,26 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color(0xFF8AE2CA),
-            Color(0xFF1B80DB),
-          ],
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-                color: Colors.amber,
-              )
+    return SafeArea(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF8AE2CA),
+              Color(0xFF1B80DB),
             ],
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: Column(
+              children: const [
+                NowPlaying(),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/now_playing.dart
+++ lib/widgets/now_playing.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:movie_macaron/bloc/get_now_playing_bloc.dart';
+import 'package:movie_macaron/model/movie_response.dart';
+import 'package:movie_macaron/model/movie.dart';
 
 class NowPlaying extends StatefulWidget {
-  const NowPlaying({required Key key}) : super(key: key);
+  const NowPlaying({Key? key}) : super(key: key);
 
   @override
   State<NowPlaying> createState() => _NowPlayingState();
@@ -19,8 +21,52 @@ class _NowPlayingState extends State<NowPlaying> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return StreamBuilder<MovieResponse> (
+      stream: nowPlayingMoviesBloc.movieObservable,
+      builder: (BuildContext context, AsyncSnapshot<MovieResponse> snapshot) {
+        if (snapshot.hasError) {
+          return _buildErrorWidget(snapshot.error as String);
+        } else if (snapshot.hasData) {
+          if (snapshot.data!.error.isNotEmpty) {
+            return _buildErrorWidget(snapshot.data!.error);
+          }
+          return _buildNowPlayingWidget(snapshot.data!);
+        } else {
+          return _buildLoadingWidget();
+        }
+
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Text('没有Stream');
+          case ConnectionState.waiting:
+            return Text('等待数据...');
+          case ConnectionState.active:
+            return Text('active: ${snapshot.data}');
+          case ConnectionState.done:
+            return Text('Stream 已关闭');
+        }
+      },
+    );
+  }
+
+  Widget _buildLoadingWidget() {
+    return const Text(" <Now Playing> is loading ...");
+  }
+
+  Widget _buildErrorWidget(String errorMsg) {
+    return Text(errorMsg);
+  }
+
+  Widget _buildNowPlayingWidget(MovieResponse data) {
+    List<Movie> movies = data.movies;
+    if (movies.isEmpty) {
+      return const Text("No movies");
+    } else {
+      for (int i=0;i<movies.length;i++) {
+        print(movies[i].title);
+      }
+      return const Text("Now Playing movies");
+    }
   }
 
 }
